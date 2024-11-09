c3271e72ac76c9bcacb36de4745f83160bed57d0
Deshon Stout <46934055+DeshonS@users.noreply.github.com>
2024-11-09 10:11:50 -0600

    completed task 3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,14 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        if cls in classes.values():
+            key = "{}.{}".format(cls.__name__, id)
+            return self.__objects.get(key)
+        return None
+    
+    def count(self, cls=None):
+        if cls:
+            return len(self.all(cls))
+        return len(self.__objects)
