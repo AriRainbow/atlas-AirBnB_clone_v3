a0193f81245acf53ef3ae4fd69a72cda637086ed
AriRainbow <mariel.williams@atlasschool.com>
2024-11-10 20:37:12 -0600

    PEP*

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ This module contains the route that returns the status of the API.
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns the status of the API"""
