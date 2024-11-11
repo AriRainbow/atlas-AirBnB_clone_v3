c26d16438efedddfb5b14b17d251fccf9cdf4bc5
AriRainbow <mariel.williams@atlasschool.com>
2024-11-10 20:34:35 -0600

    PEP 8

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,13 +5,16 @@ from models import storage
 from api.v1.views import app_views
 import os
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown(exception):
     """Close the storage session after each request"""
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
