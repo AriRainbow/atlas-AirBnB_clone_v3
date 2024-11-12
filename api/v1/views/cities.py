5213ce6f78bdcf551be3cfd2450518efd687e753
Deshon Stout <46934055+DeshonS@users.noreply.github.com>
2024-11-12 08:15:25 -0600

    reverted all changes

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,6 +6,7 @@ from models.city import City
 from models.state import State
 from api.v1.views import app_views
 
+
 @app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
 def get_cities(state_id):
     """Retrieves the list of all City objects of a State"""
@@ -14,6 +15,7 @@ def get_cities(state_id):
         abort(404)
     return jsonify([city.to_dict() for city in state.cities])
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     """Retrieves a City object"""
@@ -21,6 +23,7 @@ def get_city(city_id):
     if not city:
         abort(404)
     return jsonify(city.to_dict())
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
@@ -32,22 +35,25 @@ def delete_city(city_id):
     storage.save()
     return jsonify({}), 200
 
+
 @app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
 def create_city(state_id):
     """Creates a City object"""
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    if not request.is_json():
+    if not request.get_json():
         return make_response(jsonify({"error": "Not a JSON"}), 400)
-    data = request.get_json()
-    if 'name' not in data:
+    if 'name' not in request.get_json():
         return make_response(jsonify({"error": "Missing name"}), 400)
     
-    new_city = City(name=data['name'], state_id=state_id)
+    data = request.get_json()
+    data['state_id'] = state_id
+    new_city = City(**data)
     storage.new(new_city)
     storage.save()
     return jsonify(new_city.to_dict()), 201
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
@@ -57,7 +63,6 @@ def update_city(city_id):
         abort(404)
     if not request.get_json():
         return make_response(jsonify({"error": "Not a JSON"}), 400)
-
     data = request.get_json()
     ignore_keys = ['id', 'state_id', 'created_at', 'updated_at']
     for key, value in data.items():
